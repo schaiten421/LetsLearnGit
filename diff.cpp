ad3994c38fa53b557de0f2be4e339867093f1531
Chaitanya Sharma <schaiten421@gmail.com>
2023-06-24 23:55:09 +0530

    check both integers

--- diff.cpp
+++ diff.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main(){
-    int a,b;
+    int a,b,c=0;
     cin>>a>>b;
-    int c=a-b;
+    if(a>b)
+    c=a-b;
+    else
+    c=b-a;
     cout<<"the diff is "<<c;
     return 0;
 }
